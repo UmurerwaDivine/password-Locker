6aa049b2d3e29fc04046ebda98bdbcbb23ed8dd7
UmurerwaDivine <divinekagame@gmail.com>
2019-02-12 09:21:30 +0200

    new function of creating new account

--- password_test.py
+++ password_test.py
@@ -86,7 +86,7 @@ class TestUser(unittest.TestCase):
 
     #     self.assertTrue(account_exists)
 
-    # def test_display_all_accounts(self): 
+    # def test_display_all_accounts(self):  
     #     '''
     #     method that returns a list of all contacts saved
     #     '''
